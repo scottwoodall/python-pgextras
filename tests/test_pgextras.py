6696b30e513b20919bcdb0575b86290451079a0f
Scott Woodall <scott.woodall@gmail.com>
2014-04-29 14:29:46 -0400

    Uni test for testing blocking sql statements.

--- tests/test_pgextras.py
+++ tests/test_pgextras.py
@@ -11,7 +11,8 @@ from pgextras import PgExtras, sql_constants as sql
 
 class TestPgextras(unittest.TestCase):
     def setUp(self):
-        self.dsn = 'dbname=pgextras_unittest'
+        self.dbname = 'pgextras_unittest'
+        self.dsn = 'dbname={dbname}'.format(dbname=self.dbname)
         self.conn = psycopg2.connect(
             self.dsn,
             cursor_factory=psycopg2.extras.NamedTupleCursor
@@ -74,6 +75,43 @@ class TestPgextras(unittest.TestCase):
                 results = func()
 
                 self.assertTrue(len(results), number_of_pgbench_tables)
+
+    def test_blocking(self):
+        statement = """
+            UPDATE pgbench_branches
+            SET bbalance = bbalance + 10
+            WHERE bid = 1
+        """
+
+        blocking_conn = psycopg2.connect(
+            database=self.dbname,
+            cursor_factory=psycopg2.extras.NamedTupleCursor
+        )
+
+        blocking_cursor = blocking_conn.cursor()
+        blocking_cursor.execute(statement)
+
+        async_conn = psycopg2.connect(
+            database=self.dbname,
+            cursor_factory=psycopg2.extras.NamedTupleCursor,
+            async=1
+        )
+
+        psycopg2.extras.wait_select(async_conn)
+        async_cursor = async_conn.cursor()
+        async_cursor.execute(statement)
+
+        with PgExtras(dsn=self.dsn) as pg:
+            results = pg.blocking()
+
+        blocking_cursor.close()
+        blocking_conn.close()
+
+        while not async_conn.isexecuting():
+            async_cursor.close()
+            async_conn.close()
+
+        self.assertEqual(len(results), 1)
 
     def tearDown(self):
         self.cursor.close()
