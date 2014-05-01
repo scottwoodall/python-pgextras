5694cacbc005b07a1d6a5667c7f3d142cf1a6457
Scott Woodall <scott.woodall@gmail.com>
2014-05-01 14:31:34 -0400

    Bug fix for unit test when run against postgres version less than 9.2

--- tests/test_pgextras.py
+++ tests/test_pgextras.py
@@ -93,16 +93,17 @@ class TestPgextras(unittest.TestCase):
 
                 self.assertTrue(len(results), 4)
 
-    def test_methods_have_ten_results(self):
-        self.create_pg_stat_statement()
-        method_names = ['calls']
-
+    def test_calls(self):
         with PgExtras(dsn=self.dsn) as pg:
-            for method_name in method_names:
-                func = getattr(pg, method_name)
-                results = func()
+            if pg.is_pg_at_least_nine_two():
+                self.create_pg_stat_statement()
 
+            results = pg.calls()
+
+            if pg.pg_stat_statement():
                 self.assertTrue(len(results), 10)
+            else:
+                self.assertTrue(len(results), 0)
 
     def test_blocking(self):
         statement = """
